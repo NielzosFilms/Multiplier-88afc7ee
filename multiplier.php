928ab6dcad0f4bf44fadf43eff1c0273e47c396f
NielzosFilms <nielzosfilms@gmail.com>
2020-01-14 11:57:42 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,7 @@
+<?php
+echo("Ik ga 2 nummers vermenigvuldigen".PHP_EOL);
+echo("Nummer 1:".PHP_EOL);
+$num1 = readline();
+echo("Nummer 2:".PHP_EOL);
+$num2 = readline();
+echo("Uw resultaat is: ".($num1*$num2));
